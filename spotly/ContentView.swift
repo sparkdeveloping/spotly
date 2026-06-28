c2a3ed6e86df9290fa34cf2f9c7b0cd6017fe8fe
Denzel Nyatsanza <sbj.developing@gmail.com>
2026-06-28 00:35:36 -0500

    Initial Commit

---
+++ spotly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  spotly
+//
+//  Created by Denzel Nyatsanza on 6/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ spotly/spotlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotlyApp.swift
+//  spotly
+//
+//  Created by Denzel Nyatsanza on 6/28/26.
+//
+
+import SwiftUI
+
+@main
+struct spotlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
